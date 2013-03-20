a9ea10d90ad62327cff9f2277ea8ed445e1542a0
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-20 21:00:57 +0000

    Added license URL & description

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject rm-hull/turtle "0.1.0-SNAPSHOT"
   :description "A ClojureScript turtle-graphics library"
   :url "https://github.com/rm_hull/turtle"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1586"]
                  [jayq "2.3.0"]
@@ -12,4 +14,5 @@
   :cljsbuild {
     :builds [{:source-paths ["cljs-src"], :id "main", :jar true}]}
   :min-lein-version "2.1.0"
+  :description "A lightweight turtle drawing library for HTML5 canvas" 
 ) 
