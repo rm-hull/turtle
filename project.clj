9aa72a447e736ff3793a5993058a89ffb6453353
Richard Hull <rm_hull@yahoo.co.uk>
2013-03-23 18:30:18 +0000

    move away from snapshot releases

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rm-hull/turtle "0.1.0-SNAPSHOT"
+(defproject rm-hull/turtle "0.1.1"
   :description "A ClojureScript turtle-graphics library"
   :url "https://github.com/rm_hull/turtle"
   :license {:name "Eclipse Public License"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1586"]
                  [jayq "2.3.0"]
-                 [rm-hull/monet "0.1.6-SNAPSHOT"]]
+                 [rm-hull/monet "0.1.6"]]
   :plugins [[lein-cljsbuild "0.3.0"]
             [com.birdseye-sw/lein-dalap "0.1.0"]]
   :hooks [leiningen.dalap leiningen.cljsbuild]
