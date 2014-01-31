ca76e95d85b3f0dc9167d95f71828d342be3521e
Richard Hull <rm_hull@yahoo.co.uk>
2014-01-31 01:22:04 +0000

    Version updates

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
-(defproject rm-hull/turtle "0.1.8"
+(defproject rm-hull/turtle "0.1.9-SNAPSHOT"
   :description "A lightweight Clojure/ClojureScript turtle-graphics library for SVG, Graphics2D and HTML5 canvas rendering"
   :url "https://github.com/rm_hull/turtle"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-1934"]
-                 [prismatic/dommy "0.1.2"]
-                 [hiccup "1.0.4"]
-                 [jayq "2.4.0"]
-                 [rm-hull/monet "0.1.9"]]
-  :plugins [[lein-cljsbuild "0.3.0"]
+                 [org.clojure/clojurescript "0.0-2156"]
+                 [rm-hull/dommy "0.1.3-SNAPSHOT"]
+                 [hiccup "1.0.5"]
+                 [jayq "2.5.0"]
+                 [rm-hull/monet "0.1.10"]]
+  :plugins [[lein-cljsbuild "1.0.1"]
             [com.birdseye-sw/lein-dalap "0.1.0"]]
   :hooks [leiningen.dalap]
   :source-paths ["src" "target/generated-src"]
