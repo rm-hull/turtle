675768306892d1f7f9b282fb156e77319926567c
Richard Hull <rm_hull@yahoo.co.uk>
2016-04-07 12:52:25 +0100

    Add basic test

---
+++ test/turtle/renderer/vector_test.clj
@@ -0,0 +1,9 @@
+(ns turtle.renderer.vector-test
+  (:require
+    [clojure.test :refer :all]
+    [turtle.core :refer [draw!]]
+    [turtle.renderer.vector :refer [->svg]]))
+
+(deftest check-svg-generation
+  (let [triangle (take 12 (cycle [:fwd 20, :right 120]))]
+    (is (= (slurp "test/triangle.svg") (draw! ->svg triangle [800 600])))))
