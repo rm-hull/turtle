6e808ccf39a101f468a40ae366d9666ce5d2e78e
Richard Hull <rm_hull@yahoo.co.uk>
2016-11-05 21:09:16 +0000

    Fix typo

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
     :url "http://opensource.org/licenses/MIT"}
   :dependencies [
     [org.clojure/clojure "1.8.0"]
-    [org.clojure/clojurescript "1.9 293"]
+    [org.clojure/clojurescript "1.9.293"]
     [hipo "0.5.2"]
     [hiccup "1.0.5"]
     [rm-hull/monet "0.3.0"]]
