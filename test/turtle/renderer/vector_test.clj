8d149601603ed5692a7c504695123a1c829fe2b1
Richard Hull <rm_hull@yahoo.co.uk>
2017-12-23 21:35:58 +0000

    Fix eclint violations

--- test/turtle/renderer/vector_test.clj
+++ test/turtle/renderer/vector_test.clj
@@ -1,9 +1,10 @@
 (ns turtle.renderer.vector-test
   (:require
    [clojure.test :refer :all]
+   [clojure.string :as s]
    [turtle.core :refer [draw!]]
    [turtle.renderer.vector :refer [->svg]]))
 
 (deftest check-svg-generation
   (let [triangle (take 12 (cycle [:fwd 200, :right 120]))]
-    (is (= (slurp "test/triangle.svg") (draw! ->svg triangle [800 600])))))
+    (is (= (s/trim (slurp "test/triangle.svg")) (draw! ->svg triangle [800 600])))))
